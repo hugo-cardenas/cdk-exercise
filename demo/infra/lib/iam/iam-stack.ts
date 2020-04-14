1a35bf5549e1387729ff8b6273c642b74850e50c
hugo-cardenas <hugo.cardenas@newthings.co>
2020-04-14 18:00:58 +0300

    Play with iam stack

--- demo/infra/lib/iam/iam-stack.ts
+++ demo/infra/lib/iam/iam-stack.ts
@@ -1,27 +1,32 @@
 import * as cdk from "@aws-cdk/core";
 import * as iam from "@aws-cdk/aws-iam";
 import { users } from "./users";
+import { Stack } from "@aws-cdk/core";
 
 export class IamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const group = new iam.Group(this, "DeveloperUserGroup");
-
-    const iamUsers = users.map(
+    const developerUserGroup = new iam.Group(this, "DeveloperUserGroup");
+    const developerUsers = users.map(
       (user) => new iam.User(this, `DeveloperUser-${user.name}`)
     );
+    developerUsers.forEach((iamUser) => developerUserGroup.addUser(iamUser));
 
-    iamUsers.forEach((iamUser) => group.addUser(iamUser));
+    const adminRole = new iam.Role(this, "AdminRole", {
+      // TODO This should be restricted with a Network whitelist?
+      assumedBy: new iam.AccountPrincipal(Stack.of(this).account),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess")]
+    })
 
-    const ecrAccessPolicy = new iam.ManagedPolicy(
-      this,
-      "EcrDeveloperAccess",
-      {}
-    );
+    // const ecrAccessPolicy = new iam.ManagedPolicy(
+    //   this,
+    //   "EcrDeveloperAccess",
+    //   {}
+    // );
 
-    group.addManagedPolicy(
-      new iam.ManagedPolicy(this, "AdministratorAccess", {})
-    );
+    // developerUserGroup.addManagedPolicy(
+    //   new iam.ManagedPolicy(this, "AdministratorAccess", {})
+    // );
   }
 }
